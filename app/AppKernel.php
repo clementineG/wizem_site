7d311449ad8d93eebac288034585491789cbf9df
mathieucap <mat.capdeville@gmail.com>
2015-12-03 15:49:45 +0100

    ajout CORS bundle pour autoriser connexion à l'API

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -24,6 +24,7 @@ class AppKernel extends Kernel
             new wizem\ApiBundle\wizemApiBundle(),
             new wizem\EventBundle\wizemEventBundle(),
             new Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle(),
+            new Nelmio\CorsBundle\NelmioCorsBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
